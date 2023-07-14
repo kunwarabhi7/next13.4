264ed119501c854b6ae8774260d17faec2ecbde1
Abhishek <abhishekparmar162@gmail.com>
2023-07-14 13:41:40 +0530

    added ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>yo</div>
+    <div className='flex flex-col items-center justify-center'>
+ <h1 className='mt-40'>Enter Your Name</h1>     
+ <form className='flex flex-col'>
+  <input type="text" placeholder='Enter your name' className='my-4' />
+<button className="bg-cyan-600 w-20 mx-16 rounded-full" type='submit'>Enter</button>
+ </form>
+    </div>
   )
 }
